3d86efad30a76ee48d425fe99131a27c1cbff003
Roshan <webdev404.error@gmail.com>
2023-12-03 16:28:49 +0530

    set up the .env, backend on appwite.

---
+++ src/conf/conf.js
@@ -0,0 +1,12 @@
+const conf ={
+    appwriteUrl: String(import.meta.env.VITE_APPWRITE_URL),
+    appwriteProjectId:String(import.meta.env.VITE_APPWRITE_PROJECT_ID),
+    appwriteDbId:String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
+    appwriteCollectionId:String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
+    appwriteBucketId:String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+
+}
+
+export default conf
+
+// some times importing .env variables get tricky, it can return pure numeric values or can return null and in that case our app may crash. So we are making this file.
