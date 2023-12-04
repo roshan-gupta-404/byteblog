7f3b1e99d29e190f22243898e3491f91a8a91685
Roshan <webdev404.error@gmail.com>
2023-12-04 18:46:41 +0530

    coded post and file related functionality.

---
+++ src/appwrite/config.js
@@ -0,0 +1,129 @@
+// major appwrite configurations
+
+import conf from "../conf/conf";
+import { Client, Databases,Storage,Query, ID } from "appwrite";
+
+export class Service{
+    client = new Client();
+    databases;
+    bucket;
+
+    constructor(){
+        this.client
+        .setEndpoint(conf.appwriteUrl)
+        .setProject(conf.appwriteProjectId);
+        this.databases = new Databases(this.client);
+        this.bucket = new Storage(this.client);
+    }
+
+    // POST RELATED SERVICES
+    async createPost({title,slug,content,featuredImage,status,userId}){
+        try {
+            return await this.databases.createDocument(
+                conf.appwriteDbId,
+                conf.appwriteCollectionId,
+                slug, // i am using slug as unique id which createDocument demands
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: createPost :: error", error);
+        }
+    }
+
+    async updatPost(slug,{title,content,featuredImage,status}){
+        try {
+            return await this.databases.updateDocument(
+                conf.appwriteDbId,
+                conf.appwriteCollectionId,
+                slug, // i am using slug as unique id which createDocument demands
+                {
+                    title,
+                    content,
+                    featuredImage,
+                    status,
+                    userId
+                }
+            )
+        } catch (error) {
+            console.log("Appwrite service :: updatPost :: error", error);
+        }
+    }
+
+    async deletePost(slug){
+        try {
+            await this.databases.deleteDocument(
+                conf.appwriteDbId,
+                conf.appwriteCollectionId,
+                slug
+            )
+            return true;
+        } catch (error) {
+            console.log("Appwrite service :: deletePost :: error", error);
+            return false;
+        }
+    }
+    async getPost(slug){
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDbId,
+                conf.appwriteCollectionId,
+                slug
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPost :: error", error);
+            return false;
+        }
+    }
+
+    async getPosts(queries = [Query.equal("status","active")]){ // status is key and active is value which is to be matched.
+        try {
+            return await this.databases.getDocument(
+                conf.appwriteDbId,
+                conf.appwriteCollectionId,
+                queries
+                // we can pass other values like limits etc...
+            )
+        } catch (error) {
+            console.log("Appwrite service :: getPosts :: error", error);
+            return false;
+        }
+    }
+
+    // FILE UPLOAD SERVICES.
+    async uploadFile(file){
+        try {
+            return await this.bucket.createFile(
+                conf.appwriteBucketId,
+                ID.unique(),
+                file
+            )
+        } catch (error) {
+            console.log("Appwrite service :: uploadFile :: error", error);
+            return false;
+        }
+    }
+
+    async deleteFile(fileId){
+        try {
+            return await this.bucket.deleteFile(
+                conf.appwriteBucketId,
+                fileId
+            )
+        } catch (error) {
+            
+        }
+    }
+
+    getFilePreview(fileId){
+            return this.bucket.getFilePreview(
+                conf.appwriteBucketId,
+                fileId
+            )
+    }
+}
