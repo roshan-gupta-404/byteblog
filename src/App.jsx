b8663ee9da055350b28d98f8157d0f3386f2576f
Roshan <webdev404.error@gmail.com>
2023-12-05 14:49:41 +0530

    configured store and redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,41 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import conf from './conf/conf';
-
+import { useDispatch } from 'react-redux';
+import authService from './appwrite/auth'
+import { login, logout } from './store/authSlice'
+import { Header, Footer } from './components/index'
+import { Outlet } from 'react-router-dom';
 function App() {
-  const [count, setCount] = useState(0)
-  console.log(conf.appwriteUrl);
+  const [loading, setLoading] = useState(true) // setting loading initialy true, we will set it false when useEffect complete its work.
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1> Byte Blog</h1>
-    </>
-  )
+  useEffect(() => {
+    authService.getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          useDispatch(login({ userData }))
+        }
+        else {
+          dispatch(logout()) // if login fails then calling logout just to update the state. its a good practice.
+        }
+      })
+      .finally(() => setLoading(false))
+  }, [])
+
+  return !loading ?
+    (
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+          TODO: {/* <Outlet/> it will be from react router dom*/}
+        </main>
+        <Footer />
+      </div>
+    </div>
+    )
+    : null
 }
 
 export default App
